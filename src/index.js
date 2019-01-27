8398388cd9e1332a700da9e2a5aa817d15151bba
Hamzah Malik <hamzahrmalik@yahoo.co.uk>
2019-01-27 01:15:14 +0000

    Frontend

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { UserIsAuthenticated, UserIsNotAuthenticated } from './util/wrappers.js'
 import getWeb3 from './util/web3/getWeb3'
 
 // Layouts
-import App from './App'
+import App from './Frontend'
 import Home from './layouts/home/Home'
 import Dashboard from './layouts/dashboard/Dashboard'
 import SignUp from './user/layouts/signup/SignUp'
