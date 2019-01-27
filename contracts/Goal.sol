c9868ce30dad1b75d5e6cccddc307023f940e512
CharlieHewitt <33226685+CharlieHewitt@users.noreply.github.com>
2019-01-27 01:29:00 +0000

    Add files via upload

--- contracts/Goal.sol
+++ contracts/Goal.sol
@@ -1,12 +1,17 @@
 pragma solidity >=0.4.22 <0.6.0;
 contract GoalContract
 {
-    address payable userAddress; // users address, for returning money
+    address payable userAddress; // user's address, for returning money
     uint64 amount; // amount of currency stored
     string goalDescription; // brief description
     uint64 deadline; // Sort it out to be hours / convert to unix epoch //diff types of goals
     address payable forfeitAddress; // address for money to be sent to on forfeit
     WitnessData[] witnesses; // array of "witnesses"
+
+    /* If all witnesses voted, this would give the number of voters necessary; used to calculate proportion of
+		(i) total number of judges who voted True for early condition, or
+		(ii) voting judges who voted True for deadline condition */
+	uint8 minWitnessesIfAllVote;
 
     struct WitnessData
     {
@@ -31,7 +36,7 @@ contract GoalContract
         }
     }
 
-    /// Lets the user vote if they are a witness and havent voted yet.
+    /// Lets the user vote if they are a witness and haven't voted yet.
     function vote(uint32 _vote) public
     {
         bool voted = false;
@@ -67,5 +72,47 @@ contract GoalContract
 
     function forfeit() public payable {
         forfeitAddress.transfer(amount);
+    }
+
+    // Stuff I'm playing with
+    function countTrueVotes() public view returns(uint) { // Counts total number of judges that voted True (i.e. 1)
+        uint count = 0;
+    	for (uint i=0; i < witnesses.length ; i++){
+    		if (witnesses[i].voted == true && witnesses[i].vote == 1) {
+    			count++;
+    		}
+    	}
+    return count;
+    }
+
+	function countTotalVotes() public view returns(uint){ // Counts total number of judges that voted (either way)
+	    uint count = 0;
+		for (uint i=0; i < witnesses.length; i++){
+			if (witnesses[i].voted == true){
+				count++;
+			}
+		}
+	return count;
+	}
+
+	function earlyCondition() public { // Checks if there are already enough positive votes to trigger return of money
+		if (((1000*countTrueVotes()) / witnesses.length) >= ((1000*minWitnessesIfAllVote) / witnesses.length)) {
+			returnToUser();
+			/* I don't know if I'm accessing these variables correctly, or if the division or comparison will work.
+			(From what I can understand, floats can be evaluated but not assigned to variables.)
+			The idea is to return the money if the proportion of total witnesses who have voted True (i.e. 1) meets or exceeds the proportion.
+			This function could be called each time a witness votes. */
+		}
+	}
+
+	/// timer still to be implemented
+	function deadlineCondition() public {
+	//	if (timeCondition) { // pseudo-code; this is the function that should be called at the deadline.
+			if (((1000*countTrueVotes() / countTotalVotes()) >= (1000*minWitnessesIfAllVote / witnesses.length)) && (countTotalVotes() > 0)){
+				returnToUser();
+			} else {
+				forfeit();
+			}
+	//	}
     }
 }
